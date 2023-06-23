d58ef7869345850642d7bbe6f677c30f9e336b5f
Aphatheology <mustapha.abdulkareem@saritay.com>
2023-06-23 21:44:44 +0100

    Getting started

---
+++ README.md
@@ -0,0 +1,47 @@
+# Nodejs MongoDB Template
+
+A repository built to kickstart Nodejs/Express Project, it contains
+- Basic Authentication: Register and Login with Email and Password
+- Most used utilities
+
+## Built With
+- Node
+- Express
+- MongoDB
+- Mongoose
+
+## Quick Start
+
+Clone the repo:
+
+```bash
+git clone https://github.com/Aphatheology/node_mongodb_template.git
+cd node_mongodb_template
+```
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+Set the environment variables:
+
+```bash
+cp .envExample .env
+
+# open .env and modify the environment variables 
+```
+
+To run the project:
+
+```bash
+npm run dev
+# will run the server with node --watch
+# if you are using old node version, install nodemon and edit the dev script
+
+npm run start
+# will run the server with node
+```
+
+Thanks
---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const config = require("./src/config/config");
+const morgan = require("./src/config/morgan");
+const passport = require("passport");
+const { errorConverter, errorHandler } = require("./src/middleware/error");
+const userRoute = require("./src/users/user.route");
+require("./src/config/passport");
+
+const app = express();
+
+if (config.env !== "test") {
+    app.use(morgan.successHandler);
+    app.use(morgan.errorHandler);
+}
+app.use(express.json());
+app.use(passport.initialize());
+
+app.use("/auth", userRoute);
+
+app.get("/", (req, res) => {
+    res.send({ message: "Welcome to Nodejs MongoDB Template" });
+});
+app.use("*", (req, res) => {
+    res.send({ message: "Route Not found" });
+});
+
+app.use(errorConverter);
+
+app.use(errorHandler);
+
+module.exports = app;
